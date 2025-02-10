51a91f7fd42992d73624f3a1d20308b49882f76c
Mamonde02 <valerosojohnraymund@gmail.com>
2025-02-10 10:08:13 +0800

    add testing server fetch api

--- tsuperAppAPI/server.js
+++ tsuperAppAPI/server.js
@@ -5,8 +5,17 @@ var app = require("./app");
 //var port = process.env.PORT || 8084;
 var port = 8089
 
-app.listen(port, function(){
+app.listen(port, function () {
   console.log("App listening on port " + port);
 });
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Tsuper App API!!:)");
+});
+
+app.get("/tsuper", (req, res) => {
+  res.send("TESTING Tsuper App API!!:)");
+});
+
 
 require('./routes')(app);
