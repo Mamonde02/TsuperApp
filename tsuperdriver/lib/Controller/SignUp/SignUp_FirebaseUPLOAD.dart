d3656024dca4cb3d555d6ecb3d78f4ee6138dd54
Mamonde02 <valerosojohnraymund@gmail.com>
2025-02-15 19:15:01 +0800

    uploadDriverLicense convert as bytes for web

--- tsuperdriver/lib/Controller/SignUp/SignUp_FirebaseUPLOAD.dart
+++ tsuperdriver/lib/Controller/SignUp/SignUp_FirebaseUPLOAD.dart
@@ -7,25 +7,78 @@ extension extFirebaesUPLOAD on _SignUpVC {
         contentType: 'image/jpeg',
         customMetadata: {'picked-file-path': driverLicenseFront[0].path},
       );
-      storageRef = FirebaseStorage.instance.ref("driver and car/${driverLicenseFront[1]}.jpg");
-      final firebaseUpld = await storageRef.putFile(File(driverLicenseFront[0].path), metadata);
-      final imageURL = await firebaseUpld.ref.getDownloadURL();
+
+      XFile? frontFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+       if (frontFile == null) return; // Cancelled
+
+      Uint8List frontData = await frontFile.readAsBytes();
+
+
+      storageRef = FirebaseStorage.instance.ref("driver and car/${frontFile.name}.jpg");
+      UploadTask frontUploadTask = storageRef.putData(frontData, metadata);
+      String imageURL = await (await frontUploadTask).ref.getDownloadURL();
       legalInfo["DriverLiceneseFront"] = imageURL;
+
+
+      // mobile version only
+      // storageRef = FirebaseStorage.instance.ref("driver and car/${driverLicenseFront[1]}.jpg");
+      // final firebaseUpld = await storageRef.putFile(File(driverLicenseFront[0].path), metadata);
+      // final imageURL = await firebaseUpld.ref.getDownloadURL();
+      // legalInfo["DriverLiceneseFront"] = imageURL;
   
     } on firebase_core.FirebaseException catch (e) {
       print(e);
       return;
     }
 
+
+   // driverlicense
     try {
       final metadata = SettableMetadata(
         contentType: 'image/jpeg',
         customMetadata: {'picked-file-path': driverLicenseBack[0].path},
       );
-      storageRef = FirebaseStorage.instance.ref("driver and car/${driverLicenseBack[1]}.jpg");
-      final firebaseUpld = await storageRef.putFile(File(driverLicenseBack[0].path), metadata);
-      final imageURL = await firebaseUpld.ref.getDownloadURL();
+
+       XFile? backFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+       if (backFile == null) return; // Cancelled
+
+       Uint8List backData = await backFile.readAsBytes();
+
+
+      storageRef = FirebaseStorage.instance.ref("driver and car/${backFile.name}.jpg");
+      UploadTask backUploadTask = storageRef.putData(backData, metadata);
+      String imageURL = await (await backUploadTask).ref.getDownloadURL();
       legalInfo["DriverLiceneseBack"] = imageURL;
+      
+      print("Parent PARAMENTER D-BACK LEGALINFO : ${legalInfo}");
+
+
+
+       
+      // up reference
+      // storageRef = FirebaseStorage.instance.ref("driver and car/${frontFile.name}.jpg");
+      // UploadTask frontUploadTask = storageRef.putData(frontData, metadata);
+      // String imageURL = await (await frontUploadTask).ref.getDownloadURL();
+      // legalInfo["DriverLiceneseFront"] = imageURL;
+
+
+
+       // front old version
+            // mobile version only
+      // storageRef = FirebaseStorage.instance.ref("driver and car/${driverLicenseFront[1]}.jpg");
+      // final firebaseUpld = await storageRef.putFile(File(driverLicenseFront[0].path), metadata);
+      // final imageURL = await firebaseUpld.ref.getDownloadURL();
+      // legalInfo["DriverLiceneseFront"] = imageURL;
+  
+
+
+      // mobile version only
+      // storageRef = FirebaseStorage.instance.ref("driver and car/${driverLicenseBack[1]}.jpg");
+      // final firebaseUpld = await storageRef.putFile(File(driverLicenseBack[0].path), metadata);
+      // final imageURL = await firebaseUpld.ref.getDownloadURL();
+      // legalInfo["DriverLiceneseBack"] = imageURL;
   
     } on firebase_core.FirebaseException catch (e) {
       print(e);
@@ -39,7 +92,10 @@ extension extFirebaesUPLOAD on _SignUpVC {
         contentType: 'image/jpeg',
         customMetadata: {'picked-file-path': carRegistrationBack[0].path},
       );
-      storageRef = FirebaseStorage.instance.ref("driver and car/${carRegistrationBack[1]}.jpg");
+      storageRef = FirebaseStorage.instance.ref("driver and car/${carRegistrationBack[1]}.jpg"); // pass 
+
+      // web version storage firebase
+
       final firebaseUpld = await storageRef.putFile(File(carRegistrationBack[0].path), metadata);
       final imageURL = await firebaseUpld.ref.getDownloadURL();
       legalInfo["CarRegistrationFront"] = imageURL;
