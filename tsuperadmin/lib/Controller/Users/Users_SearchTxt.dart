913f0295cfdb667b0c03dd7f5b6acea705846a97
Mamonde02 <valerosojohnraymund@gmail.com>
2025-03-05 17:08:21 +0800

    add onChange on search widget

--- tsuperadmin/lib/Controller/Users/Users_SearchTxt.dart
+++ tsuperadmin/lib/Controller/Users/Users_SearchTxt.dart
@@ -1,17 +1,17 @@
 part of 'UsersVC.dart';
 
 extension extSearchTxt on _UsersVC {
-  Widget searchTxtView(){
-    return
-    SizedBox(
+  Widget searchTxtView() {
+    return SizedBox(
       width: 300,
       child: TextField(
         cursorColor: Colors.black,
         textAlign: TextAlign.start,
-        controller: searchTxtController,
+        controller: searchTxtController, // based
         style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
         decoration: InputDecoration(
-          contentPadding: const EdgeInsets.only(top: 12, bottom: 11, left: 25, right: 10),
+          contentPadding:
+              const EdgeInsets.only(top: 12, bottom: 11, left: 25, right: 10),
           filled: true,
           fillColor: Colors.white,
           hintStyle: TextStyle(fontSize: 15, color: Colors.grey[400]),
@@ -24,20 +24,32 @@ extension extSearchTxt on _UsersVC {
             borderSide: BorderSide(color: Colors.transparent),
           ),
         ),
-        onChanged: (value){
-          if (value.isEmpty){
-            displayedUsersData = allUsersData;
-          }else{
-            displayedUsersData =  allUsersData.where((e) {
-                                    final fullName = "${e.FirstName.toLowerCase()} ${e.LastName.toLowerCase()}";
-                                    final memberType = e.MemberType.toLowerCase() == "carowner" ? "Car Owner" : e.MemberType;
-                                    return
-                                    fullName.contains(value.toLowerCase()) ||
-                                    memberType.toLowerCase().contains(value.toLowerCase());
-                                  }).toList();
-          }
+        // onChanged: (value){
+        //   if (value.isEmpty){
+        //     displayedUsersData = allUsersData;
+        //   }else{
+        //     displayedUsersData =  allUsersData.where((e) {
+        //                             final fullName = "${e.FirstName.toLowerCase()} ${e.LastName.toLowerCase()}";
+        //                             final memberType = e.MemberType.toLowerCase() == "carowner" ? "Car Owner" : e.MemberType;
+        //                             return
+        //                             fullName.contains(value.toLowerCase()) ||
+        //                             memberType.toLowerCase().contains(value.toLowerCase());
+        //                           }).toList();
+        //   }
 
-          refreshState();
+        //   refreshState();
+        // },
+        onChanged: (value) => {
+          if (value.isEmpty)
+            {
+              // Display the all data when search text is empty
+              print("Data is empty SearchText"),
+              filteredUsers = allUsersData
+            }
+          else
+            {
+              filterUsers(),
+            }
         },
       ),
     );
