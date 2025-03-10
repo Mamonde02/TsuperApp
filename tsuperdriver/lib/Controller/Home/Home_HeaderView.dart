4381b76329532509e50f2c7689481e37ab7219ce
Mamonde02 <valerosojohnraymund@gmail.com>
2025-03-10 21:30:36 +0800

    add condition header if new acc

--- tsuperdriver/lib/Controller/Home/Home_HeaderView.dart
+++ tsuperdriver/lib/Controller/Home/Home_HeaderView.dart
@@ -1,24 +1,26 @@
 part of 'HomeVC.dart';
 
 extension extHomeHeaderView on _HomeVC {
-  Widget headerTitleView(){
-    const style1 = TextStyle(fontSize: 45, color: Colors.white, fontWeight: FontWeight.w700);
+  Widget headerTitleView() {
+    const style1 = TextStyle(
+        fontSize: 45, color: Colors.white, fontWeight: FontWeight.w700);
+    const style2 = TextStyle(
+        fontSize: 19, color: Colors.white, fontWeight: FontWeight.w700);
 
-    return
-    Container(
+    return Container(
       height: 130,
       color: gkGetColor(PickClr.tsuperTheme),
-      child: 
-      Row(
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 20.5),
             child: Image.asset(AssetStoreImage.tsuperRiderLogo),
           ),
-
           const SizedBox(width: 15),
-          const Text("TSUPER", style: style1)
+          userData?.Status == "ACTIVE" // condition
+              ? const Text("Account Confirmation TSUPER", style: style2)
+              : const Text("TSUPER", style: style1)
         ],
       ),
     );
